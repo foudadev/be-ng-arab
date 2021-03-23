7756af2f119b7b8d39e2a239571736bb348679fd
AmrNRD <amrmohamed257@gmail.com>
2021-03-23 04:08:24 +0200

    mid-senior for update

--- app/Http/Requests/v1/Question/QuestionAPIUpdateFormRequest.php
+++ app/Http/Requests/v1/Question/QuestionAPIUpdateFormRequest.php
@@ -27,7 +27,7 @@ class QuestionAPIUpdateFormRequest extends FormRequest
     {
         return [
             'question' => ['required', 'string'],
-            'level' => ['required', 'string', 'in:junior,senior,expert'],
+            'level' => ['required', 'string', 'in:junior,mid-senior,senior,expert'],
             'status' => ['nullable', 'string', 'in:active,inactive'],
             'resource_link' => ['nullable', 'string'],
             'hint' => ['nullable', 'string'],
